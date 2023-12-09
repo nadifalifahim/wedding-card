ddfa5c0f4c1a8de8ab8335fd76d37925e34fc9ea
Nadif Ali Fahim <nadifalifahim@gmail.com>
2023-12-09 22:00:39 +0600

    music fix

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 let bg_music = document.getElementById("bg-music");
 let bg_music_icon = document.getElementById("music-icon");
 
-bg_music.play();
-
 function music_btn_pressed() {
   if (bg_music.duration > 0 && !bg_music.paused) {
     bg_music.pause();
     bg_music_icon.src = "Resources/background music/music_status/pause.png";
   } else {
-    bg_music.play();
+    play_music();
     bg_music_icon.src = "Resources/background music/music_status/play.png";
   }
+}
+
+function play_music() {
+  bg_music.play();
 }
